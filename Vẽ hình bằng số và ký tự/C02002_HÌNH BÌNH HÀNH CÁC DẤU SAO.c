54a39466483df63a6141163c579a3e34c3411935
Trần Phú Cường <94471615+zeusptit@users.noreply.github.com>
2024-01-21 11:57:47 +0700

    Update C02002_HÌNH BÌNH HÀNH CÁC DẤU SAO.c

--- Vẽ hình bằng số và ký tự/C02002_HÌNH BÌNH HÀNH CÁC DẤU SAO.c
+++ Vẽ hình bằng số và ký tự/C02002_HÌNH BÌNH HÀNH CÁC DẤU SAO.c
@@ -6,14 +6,14 @@ int main()
     unsigned n,t;
     scanf("%u",&n);
     t=n;
-    if(t>0){
-    t--;
-    for(int i=1;i<=t;i++){
-        printf("~");
-    }
-    for(int j=1;j<=n;j++){
-        printf("*");
-    }
-    printf("\n");
+    while(t>0){
+	    t--;
+	    for(int i=1;i<=t;i++){
+	        printf("~");
+	    }
+	    for(int j=1;j<=n;j++){
+	        printf("*");
+	    }
+	    printf("\n");
     }
 }
